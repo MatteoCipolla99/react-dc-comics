1669103b969a28d605bbd39ab65391c3266d6565
Matteo Cipolla <mati_teo@live.it>
2025-01-29 17:57:42 +0100

    feat:add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,147 @@
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="container-blue">
+          <div className="blue">
+            <img src="./img/buy-comics-digital-comics.png" alt="" />
+            <a href="#">DIGITAL COMICS</a>
+          </div>
+          <div className="blue">
+            <img src="./img/buy-comics-merchandise.png" alt="" />
+            <a href="#">DC MERCHANDISE</a>
+          </div>
+          <div className="blue">
+            <img src="./img/buy-comics-subscriptions.png" alt="" />
+            <a href="#">SUBSCRIPTION</a>
+          </div>
+          <div className="blue">
+            <img src="./img/buy-comics-shop-locator.png" alt="" />
+            <a href="#">COMIC SHOP LOCATOR</a>
+          </div>
+          <div className="blue">
+            <img src="./img/buy-dc-power-visa.svg" alt="" />
+            <a href="#">DC POWER VISA</a>
+          </div>
+        </div>
+      </div>
+      <div className="central-footer">
+        <div className="container-footer">
+          <div className="first-col">
+            <h3>DC COMICS</h3>
+            <ul>
+              <li>
+                <a href="#">Characters</a>
+              </li>
+              <li>
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Games</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+            <h3 className="second-title">SHOP</h3>
+            <ul>
+              <li>
+                <a href="#">Shop DC</a>
+              </li>
+              <li>
+                <a href="#">Shop DC Collectibles</a>
+              </li>
+            </ul>
+          </div>
+          <div className="second-col">
+            <h3>DC</h3>
+            <ul>
+              <li>
+                <a href="#">Terms of use</a>
+              </li>
+              <li>
+                <a href="#">Privacy Policy (New)</a>
+              </li>
+              <li>
+                <a href="#">Ad choices</a>
+              </li>
+              <li>
+                <a href="#">Advertising</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Subscriptions</a>
+              </li>
+              <li>
+                <a href="#">Talent workshop</a>
+              </li>
+              <li>
+                <a href="#">CPSC Certificates</a>
+              </li>
+              <li>
+                <a href="#">Ratings</a>
+              </li>
+              <li>
+                <a href="#">Shop Help</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div className="third-col">
+            <h3 className="third-title">SITES</h3>
+            <ul>
+              <li>
+                <a href="#">DC</a>
+              </li>
+              <li>
+                <a href="#">MAD Magazine</a>
+              </li>
+              <li>
+                <a href="#">DC Kids</a>
+              </li>
+              <li>
+                <a href="#">DC Universe</a>
+              </li>
+              <li>
+                <a href="#">DC Power Visa</a>
+              </li>
+            </ul>
+          </div>
+          <div className="img-footer">
+            <img src="./img/dc-logo-bg.png" alt="logo" />
+          </div>
+        </div>
+      </div>
+      <div className="ultimate-footer">
+        <div className="container-ultimate-footer">
+          <div className="left-side">
+            <button className="sign-up">SIGN-UP NOW!</button>
+          </div>
+          <div className="right-side">
+            <h3>FOLLOW-US</h3>
+            <div className="img-ultimate-footer">
+              <img src="./img/footer-facebook.png" alt="" />
+              <img src="./img/footer-twitter.png" alt="" />
+              <img src="./img/footer-youtube.png" alt="" />
+              <img src="./img/footer-pinterest.png" alt="" />
+              <img src="./img/footer-periscope.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
