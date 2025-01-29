7763c4417397fdea1075c34b16ef712fe4a50c89
Matteo Cipolla <mati_teo@live.it>
2025-01-29 16:21:07 +0100

    feat:add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,44 @@
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <div className="container">
+      <img className="logo-dc" src="./img/dc-logo.png" alt="logo-dc"></img>
+      <nav className="nav-bar">
+        <ul>
+          <li>
+            <a href="#">CHARACTERS</a>
+          </li>
+          <li>
+            <a id="button" href="#">
+              COMICS
+            </a>
+            <hr class name="line"></hr>
+          </li>
+          <li>
+            <a href="#">MOVIES</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">GAMES</a>
+          </li>
+          <li>
+            <a href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a href="#">FANS</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
