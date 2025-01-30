ea600dc877fdc264368763c399a2f8bed6abcff6
Matteo Cipolla <mati_teo@live.it>
2025-01-30 13:04:18 +0100

    feat:add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,18 @@
+import navBarData from "../data/navBarData";
+
 export default function Header() {
   return (
     <div className="container">
       <img className="logo-dc" src="./img/dc-logo.png" alt="logo-dc"></img>
       <nav className="nav-bar">
         <ul>
-          <li>
-            <a href="#">CHARACTERS</a>
-          </li>
-          <li>
-            <a id="button" href="#">
-              COMICS
-            </a>
-            <hr class name="line"></hr>
-          </li>
-          <li>
-            <a href="#">MOVIES</a>
-          </li>
-          <li>
-            <a href="#">TV</a>
-          </li>
-          <li>
-            <a href="#">GAMES</a>
-          </li>
-          <li>
-            <a href="#">COLLECTIBLES</a>
-          </li>
-          <li>
-            <a href="#">VIDEOS</a>
-          </li>
-          <li>
-            <a href="#">FANS</a>
-          </li>
-          <li>
-            <a href="#">NEWS</a>
-          </li>
-          <li>
-            <a href="#">SHOP</a>
-          </li>
+          {navBarData.map((navBar) => {
+            return (
+              <li key={navBarData.id} className={navBar.current ? "line" : ""}>
+                <a href={navBar.url}>{navBar.text}</a>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </div>
